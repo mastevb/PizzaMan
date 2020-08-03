525f0973cde27e1414c5a77598a5808b4af50af4
mabochen <bochenma@cs.washington.edu>
2020-08-03 00:37:50 -0700

    first commit

---
+++ src/main/java/pizzas/PizzaManApplication.java
@@ -0,0 +1,13 @@
+package pizzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaManApplication.class, args);
+    }
+
+}
